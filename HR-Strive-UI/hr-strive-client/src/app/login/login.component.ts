15f7b395922fcb80f26d83a1ed6dedd670173ab1
Kieron Peters <kieronpeters1@gmail.com>
2021-06-24 15:59:29 +0100

    Task1 added new form elements for the inputs

--- HR-Strive-UI/hr-strive-client/src/app/login/login.component.ts
+++ HR-Strive-UI/hr-strive-client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,18 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
+  }
+
+  profileForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
   }
 
 }
