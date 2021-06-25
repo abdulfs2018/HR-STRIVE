f07693a1f0971ef94e6a5860464631fee0fafb8a
Kieron Peters <kieronpeters1@gmail.com>
2021-06-25 16:09:06 +0100

    Task1 - added change validation to the form for password and email checks to enable the submit button

--- HR-Strive-UI/hr-strive-client/src/app/login/login.component.ts
+++ HR-Strive-UI/hr-strive-client/src/app/login/login.component.ts
@@ -14,14 +14,23 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  profileForm = new FormGroup({
+  // regex is for a Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+  passwordRegex : string = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+
+  profileForm : FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    password: new FormControl('', [Validators.required, Validators.pattern(this.passwordRegex)]),
   });
 
   onSubmit() {
-    // TODO: Use EventEmitter with form value
+    // TODO: Use EventEmitter with form value -- replace logic check below with API request returning true or false to redirect user to welcome page
     console.warn(this.profileForm.value);
+
+    if (this.profileForm.value.email == "kieronpeters1@gmail.com" && this.profileForm.value.password == "12345678tT@") {
+      console.warn("passed validation");
+    } else {
+      console.warn("failed validation");
+    }
   }
 
 }
