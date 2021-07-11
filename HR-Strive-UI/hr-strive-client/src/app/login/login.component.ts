a58250708322e12eb0c0e4a46628126baf938dc3
Kieron Peters <kieronpeters1@gmail.com>
2021-07-11 21:57:55 +0100

    Task1 added change to use validators to check form inputs are correct

--- HR-Strive-UI/hr-strive-client/src/app/login/login.component.ts
+++ HR-Strive-UI/hr-strive-client/src/app/login/login.component.ts
@@ -18,29 +18,39 @@ export class LoginComponent implements OnInit {
 
   validateEmail(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-      // execute check on db here (return null for passing, return some value as true to check in ngif in html to reveal a div with alert email exists already)
-      return  {alreadyExist: 'true'};
+      
+      if (control.value == "kieronpeters1@gmail.com") {
+        return  null;
+      }
+      return  {invalidEmail :'true'};
+      
     };
   }
 
-  // TODO: regex is for a Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+  validatePass(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+
+      if (control.value == "12345678tT@") {
+        return  null;
+      }
+      return  {invalidPass: 'true'};
+      
+      
+    };
+  }
+
+  // regex is for a Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
   passwordRegex : string = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
 
   profileForm : FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email, this.validateEmail()]),
-    password: new FormControl('', [Validators.required, Validators.pattern(this.passwordRegex)]),
+    password: new FormControl('', [Validators.required, Validators.pattern(this.passwordRegex), this.validatePass()]),
   });
 
   onSubmit() {
-    // TODO: Use EventEmitter with form value -- replace logic check below with API request returning true or false to redirect user to welcome page
     console.warn(this.profileForm.value);
-
-    if (this.profileForm.value.email == "kieronpeters1@gmail.com" && this.profileForm.value.password == "12345678tT@") {
-      console.warn("passed validation");
-      this.router.navigate(['logged-in']);
-    } else {
-      console.warn("failed validation");
-    }
+    console.warn("passed validation");
+    this.router.navigate(['logged-in']);
   }
 
 }
