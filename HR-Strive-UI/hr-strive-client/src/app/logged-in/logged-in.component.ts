a4d1b9d5074fd7cb5607408518b1836c4b8760c0
Kieron Peters <kieronpeters1@gmail.com>
2021-07-12 22:56:28 +0100

    Task1 added validation to correct location, added new state variable to pass through router for API token to be sent through later

--- HR-Strive-UI/hr-strive-client/src/app/logged-in/logged-in.component.ts
+++ HR-Strive-UI/hr-strive-client/src/app/logged-in/logged-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-logged-in',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoggedInComponent implements OnInit {
 
-  constructor() { }
+  example: string;
+
+  constructor(private router: Router) { 
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation?.extras.state as {example: string};
+    this.example = state.example;
+  }
 
   ngOnInit(): void {
   }
--- HR-Strive-UI/hr-strive-client/src/app/login/login.component.ts
+++ HR-Strive-UI/hr-strive-client/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, EmailValidator, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  public invalid: boolean = false;
+
   constructor(private router : Router) { }
 
 
@@ -16,41 +18,27 @@ export class LoginComponent implements OnInit {
 
   }
 
-  validateEmail(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      
-      if (control.value == "kieronpeters1@gmail.com") {
-        return  null;
-      }
-      return  {invalidEmail :'true'};
-      
-    };
-  }
-
-  validatePass(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-
-      if (control.value == "12345678tT@") {
-        return  null;
-      }
-      return  {invalidPass: 'true'};
-      
-      
-    };
-  }
-
   // regex is for a Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
   passwordRegex : string = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
 
   profileForm : FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email, this.validateEmail()]),
-    password: new FormControl('', [Validators.required, Validators.pattern(this.passwordRegex), this.validatePass()]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(this.passwordRegex)]),
   });
 
   onSubmit() {
+
     console.warn(this.profileForm.value);
-    console.warn("passed validation");
-    this.router.navigate(['logged-in']);
+
+    // replace with API validation call
+    if (this.profileForm.value.email == "kieronpeters1@gmail.com") {
+      console.warn("passed validation");
+      // replace data example string with generated login token from API
+      const navigationExtras: NavigationExtras = {state: {example: 'This is an example'}};
+      this.router.navigate(['logged-in'], navigationExtras);
+    }
+    this.invalid = true;
+
   }
 
 }
