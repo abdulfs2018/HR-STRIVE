e7cbf5c846ca983b75e40de93aabbd208de7459b
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-06 21:03:21 +0100

    added timesheet cmpnt

--- HR-Strive-UI/hr-strive-client/src/app/modules/timesheet/timesheet.module.ts
+++ HR-Strive-UI/hr-strive-client/src/app/modules/timesheet/timesheet.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TimesheetRoutingModule } from './timesheet-routing.module';
+import { TimesheetComponent } from './timesheet/timesheet.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TimesheetComponent
+  ],
   imports: [
     CommonModule,
     TimesheetRoutingModule
