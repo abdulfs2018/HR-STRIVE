5c5ff76bc9cc4166200c85990c1f03c9d58f784e
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-06 21:02:49 +0100

    timesheet module init

---
+++ HR-Strive-UI/hr-strive-client/src/app/modules/timesheet/timesheet.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TimesheetRoutingModule } from './timesheet-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TimesheetRoutingModule
+  ]
+})
+export class TimesheetModule { }
