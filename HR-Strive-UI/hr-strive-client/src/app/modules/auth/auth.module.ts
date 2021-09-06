421e48874f9966b0eeb44891ddbdc1ebd846558d
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-06 21:16:28 +0100

    auth module init

---
+++ HR-Strive-UI/hr-strive-client/src/app/modules/auth/auth.module.ts
@@ -0,0 +1,16 @@
+//angular imports
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//app imports
+import { AuthRoutingModule } from './auth-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
