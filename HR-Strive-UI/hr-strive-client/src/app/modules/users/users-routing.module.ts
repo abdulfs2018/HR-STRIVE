8f0067fa75adac5613bfe586f3a133cfd90b2d9a
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-06 21:53:26 +0100

    forgot to use users cmpnt in routes

--- HR-Strive-UI/hr-strive-client/src/app/modules/users/users-routing.module.ts
+++ HR-Strive-UI/hr-strive-client/src/app/modules/users/users-routing.module.ts
@@ -2,7 +2,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//app imports
+import { UsersComponent } from './users/users.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
