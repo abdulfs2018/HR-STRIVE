ff84d4d47fcd070903394470ef2c661dd86d2c86
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-06 21:00:58 +0100

    added mtericz cmpnt

--- HR-Strive-UI/hr-strive-client/src/app/modules/metricz/metricz.module.ts
+++ HR-Strive-UI/hr-strive-client/src/app/modules/metricz/metricz.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MetriczRoutingModule } from './metricz-routing.module';
+import { MetriczComponent } from './metricz/metricz.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MetriczComponent
+  ],
   imports: [
     CommonModule,
     MetriczRoutingModule
