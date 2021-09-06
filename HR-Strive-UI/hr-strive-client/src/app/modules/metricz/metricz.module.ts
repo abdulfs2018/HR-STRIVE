52a97c167008e0c68eadf8c85576658293fef3d5
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-06 20:59:59 +0100

    mreticz module init

---
+++ HR-Strive-UI/hr-strive-client/src/app/modules/metricz/metricz.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MetriczRoutingModule } from './metricz-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MetriczRoutingModule
+  ]
+})
+export class MetriczModule { }
