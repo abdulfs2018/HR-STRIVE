f3c61180b4af763e56426410da8fadef90f951a3
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-07 12:16:46 +0100

    removed not needed stuff in app component

--- HR-Strive-UI/hr-strive-client/src/app/app.component.ts
+++ HR-Strive-UI/hr-strive-client/src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    /** Returns current year, e.g. 2019, 2020. */
-    readonly currentYear: number = new Date().getFullYear();
 }
