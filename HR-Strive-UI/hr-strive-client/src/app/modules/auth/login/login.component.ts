583d1c615c2088f20c34d44e06ce92ca7536ce43
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-12 21:31:28 +0100

    dummy login form first draft

--- HR-Strive-UI/hr-strive-client/src/app/modules/auth/login/login.component.ts
+++ HR-Strive-UI/hr-strive-client/src/app/modules/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 //angular imports
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,9 +10,12 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  public loginForm: FormGroup = new FormGroup({});
+
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
   //TODO: just a dummy implementation, will be replaced when auth process is in place
@@ -19,4 +23,11 @@ export class LoginComponent implements OnInit {
     this.router.navigate(['/home']);
   }
 
+  private buildForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [ null, [Validators.required, Validators.email, Validators.maxLength(255) ]],
+      password: [ null, [Validators.required, Validators.min(8), Validators.maxLength(255) ]],
+      confirmPassword: [ null, Validators.required, Validators.min(8), Validators.maxLength(255) ] //TODO: add confirm password validator
+    });
+  }
 }
