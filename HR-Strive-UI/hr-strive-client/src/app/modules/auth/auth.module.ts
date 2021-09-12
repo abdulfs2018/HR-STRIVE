e71b397871dcbac6fa7d5e20a509de1c84ccd662
m.kikmunter <M.Kikmunter@gmail.com>
2021-09-12 21:28:36 +0100

    added required imports to auth module

--- HR-Strive-UI/hr-strive-client/src/app/modules/auth/auth.module.ts
+++ HR-Strive-UI/hr-strive-client/src/app/modules/auth/auth.module.ts
@@ -1,10 +1,16 @@
 //angular imports
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 //libs imports
 import { NxButtonModule } from '@aposin/ng-aquila/button';
 import { NxGridModule } from '@aposin/ng-aquila/grid';
+import { NxInputModule } from '@aposin/ng-aquila/input';
+import { NxFormfieldModule } from '@aposin/ng-aquila/formfield';
+import { NxCardModule } from '@aposin/ng-aquila/card';
+import { NxLinkModule } from '@aposin/ng-aquila/link';
 
 //app imports
 import { AuthRoutingModule } from './auth-routing.module';
@@ -20,8 +26,14 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
+    RouterModule,
+    NxLinkModule,
     NxButtonModule,
-    NxGridModule
+    NxGridModule,
+    NxInputModule,
+    NxFormfieldModule,
+    ReactiveFormsModule,
+    NxCardModule
   ]
 })
 export class AuthModule { }
